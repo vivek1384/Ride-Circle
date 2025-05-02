ef361d9bf2e5300c803dde2ef56d3985a731ee19
vivek1384 <160699490+vivek1384@users.noreply.github.com>
2025-05-02 16:51:03 +0530

    Changes in about/how-it-works

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-about',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-how-it-works',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './how-it-works.component.html',
   styleUrl: './how-it-works.component.css'
 })
